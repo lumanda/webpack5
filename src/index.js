0990c75484e8ff179bc8f356b4257bb189038315
lumanda <854260059@qq.com>
2022-02-09 21:21:43 +0800

    设置 htmlwebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash'
-
+import printMe from './print.js'
 
 function component() {
     var element = document.createElement('div');
+    const btn = document.createElement('button');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack', '方正小标宋简体'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }

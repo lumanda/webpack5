b4e8b4c7b85c3c0647092b1c672552492f76a831
lumanda <854260059@qq.com>
2022-02-09 23:46:42 +0800

    entryPoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import _ from 'lodash'
-import printMe from './print.js'
 
 function component() {
     var element = document.createElement('div');
@@ -7,11 +6,6 @@ function component() {
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack', '方正小标宋简体'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
 
     return element;
 }

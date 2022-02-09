0188156a24b4b438dd0f18c1e2c5878ed0542073
lumanda <854260059@qq.com>
2022-02-09 21:05:18 +0800

    回退处理

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './icon.png'
-import Data from './data.xml';
-import Notes from './data.csv';
+
 
 function component() {
     var element = document.createElement('div');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack', '方正小标宋简体'], ' ');
-    element.classList.add('hello');
-
-    // 将图像添加到我们现有的 div。
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
-    console.log(Notes);
 
     return element;
 }

e3984c025826446d0cc9c8d4a0bf85ef4123352f
lumanda <854260059@qq.com>
2022-02-09 21:01:02 +0800

    加载 csv、tsv和xml数据

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash'
 import './style.css'
 import Icon from './icon.png'
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
     var element = document.createElement('div');
@@ -13,6 +15,9 @@ function component() {
     var myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
 
     return element;
 }
